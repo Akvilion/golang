91d856388ad0d72bd9bd640522938ebe5aa33b68
Akvilion <ianko.andrii@gmail.com>
2024-04-10 18:36:09 +0300

    2

--- three.go
+++ three.go
@@ -6,6 +6,13 @@ func one(word string) {
 	fmt.Println(word)
 }
 
+func two(text string, counter int) {
+	for i := 0; i < counter; i++ {
+		fmt.Println(text)
+	}
+}
+
 func main() {
 	one("hello world")
+	two("alan", 4)
 }
