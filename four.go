63d80c12b6f07d374789996fc554d4af0a816d09
Akvilion <ianko.andrii@gmail.com>
2024-04-11 09:51:48 +0300

    5

--- four.go
+++ four.go
@@ -4,11 +4,25 @@ import (
 	"fmt"
 )
 
+func one(a int, b int) (int, int, error) {
+	if a < 0 {
+		return 0, 0, fmt.Errorf("a < 0")
+	}
+	if a == 0 {
+		return 0, 0, fmt.Errorf("a == 0")
+	}
+	return a, b, nil
+}
+
 func main() {
 	// log.Fatal("something") // завершить програму після виводу
 	// fmt.Println("hello")   // це не буде виведено
 
-	err := fmt.Errorf("123")
+	// err := fmt.Errorf("123")
+	// fmt.Println(err)
+
+	a, b, err := one(-2, 4)
+	fmt.Println(a, b)
 	fmt.Println(err)
 
 }
