cd868832882a30dd209d2df8c0774e1330eca25a
Akvilion <ianko.andrii@gmail.com>
2024-04-13 10:35:00 +0300

    25

--- a15.go
+++ a15.go
@@ -5,7 +5,11 @@ import "fmt"
 func main() {
 	a := [4]int{11, 22, 33, 44}
 	fmt.Println(a)
-	for i := 0; i < 4; i++ {
+	for i := 0; i < len(a); i++ {
 		fmt.Println(i, a[i])
+	}
+
+	for index, value := range a {
+		fmt.Println(index, value)
 	}
 }
