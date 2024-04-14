82963d9833182ef36a144c1eaa8412c8b911e7cc
Akvilion <ianko.andrii@gmail.com>
2024-04-14 19:55:14 +0300

    38

--- src/github.com/headfirstgo/average2/main.go
+++ src/github.com/headfirstgo/average2/main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
 	fmt.Println(os.Args[1:])
+	var sum float64 = 0
+	arguments := os.Args[1:]
+	for _, argument := range arguments {
+		number, err := strconv.ParseFloat(argument, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		sum += number
+	}
+	fmt.Println(sum / float64(len(arguments)))
 }
