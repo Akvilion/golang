2aec00c7123652fcad2a8fb74f9d7868127ab78a
Akvilion <ianko.andrii@gmail.com>
2024-04-14 12:51:46 +0300

    35

--- src/github.com/headfirstgo/datafile/floats.go
+++ src/github.com/headfirstgo/datafile/floats.go
@@ -9,20 +9,21 @@ import (
 )
 
 // читає значення float64 з кожної строки файла
-func GetFloats(fileName string) ([3]float64, error) {
-	var numbers [3]float64
+func GetFloats(fileName string) ([]float64, error) {
+	var numbers []float64
 	file, err := os.Open(fileName)
 	if err != nil {
 		return numbers, err
 	}
-	i := 0
+	//i := 0
 	scanner := bufio.NewScanner(file) // читаємо файл в scanner
 	for scanner.Scan() {
-		numbers[i], err = strconv.ParseFloat(scanner.Text(), 64) // перетворення кожної text в float
+		number, err := strconv.ParseFloat(scanner.Text(), 64) // перетворення кожної text в float
 		if err != nil {
 			return numbers, err
 		}
-		i++
+		// i++
+		numbers = append(numbers, number)
 	}
 	err = file.Close()
 	if err != nil {
