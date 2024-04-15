c31136c1456ecedf4ee9b8998313805fe30596d2
Akvilion <ianko.andrii@gmail.com>
2024-04-15 10:21:10 +0300

    45

---
+++ src/github.com/headfirstgo/datafile/strings.go
@@ -0,0 +1,36 @@
+// package datafile
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func GetStrings(fileName string) ([]string, error) {
+	var lines []string
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	scaner := bufio.NewScanner(file)
+	for scaner.Scan() {
+		line := scaner.Text()
+		lines = append(lines, line)
+	}
+	err = file.Close()
+	if err != nil {
+		return nil, err
+	}
+	if scaner.Err() != nil {
+		return nil, scaner.Err()
+	}
+	return lines, nil
+
+}
+
+func main() {
+	res, _ := GetStrings("votes.txt")
+	fmt.Println(res)
+}
