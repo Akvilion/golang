ddb71e59902070e7e8bf6ace2af2a488aa73c1df
Akvilion <ianko.andrii@gmail.com>
2024-04-15 11:22:33 +0300

    48

--- src/github.com/headfirstgo/count/main.go
+++ src/github.com/headfirstgo/count/main.go
@@ -7,9 +7,29 @@ import (
 )
 
 func main() {
-	lines, err := datafile.GetStrings("votes.txt") // "../datafile/votes.txt"
+	lines, err := datafile.GetStrings("../datafile/votes.txt") // "../datafile/votes.txt"
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(lines)
+	// fmt.Println(lines)
+
+	var names []string
+	var counts []int
+
+	for _, line := range lines {
+		matched := false
+		for i, name := range names {
+			if line == name {
+				counts[i]++
+				matched = true
+			}
+		}
+		if !matched {
+			names = append(names, line)
+			counts = append(counts, 1)
+		}
+	}
+	for i, name := range names {
+		fmt.Printf("%s: %d\n", name, counts[i])
+	}
 }
