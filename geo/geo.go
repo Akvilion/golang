be2bd9722ca543aec33920a32a69579e9cea118b
Akvilion <ianko.andrii@gmail.com>
2024-04-23 15:41:01 +0300

    91

--- geo/geo.go
+++ geo/geo.go
@@ -4,6 +4,15 @@ type Coordinates struct {
 	Latitude  float64
 	Longitude float64
 }
+
+func (c *Coordinates) SetLatitude(latitude float64) {
+	c.Latitude = latitude
+}
+
+func (c *Coordinates) SetLongitude(longitude float64) {
+	c.Longitude = longitude
+}
+
 type Landmark struct {
 	Name string
 	Coordinates
