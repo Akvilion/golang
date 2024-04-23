74332d9eb738c482247a1c5722f769631826507c
Akvilion <ianko.andrii@gmail.com>
2024-04-23 15:29:04 +0300

    90

--- struct_encapsulation/one.go
+++ struct_encapsulation/one.go
@@ -3,17 +3,31 @@ package main
 import "fmt"
 
 type Date struct {
-	Day    int
-	Mounth int
-	Year   int
+	Day   int
+	Month int
+	Year  int
 }
 
 func (d *Date) SetYear(year int) { // метод отримує копію (d Date) -> (d *Date)
 	d.Year = year
 }
 
+func (d *Date) SetMonth(month int) { // метод отримує копію (d Date) -> (d *Date)
+	d.Month = month
+}
+
+func (d *Date) SetDay(day int) { // метод отримує копію (d Date) -> (d *Date)
+	d.Day = day
+}
+
 func main() {
+	// date := Date{}
+	// date.SetYear(1991)
+	// fmt.Println(date.Year) // 0 - виведе
+
 	date := Date{}
-	date.SetYear(1991)
-	fmt.Println(date.Year) // 0 - виведе
+	date.SetYear(2019)
+	date.SetMonth(5)
+	date.SetDay(27)
+	fmt.Println(date)
 }
