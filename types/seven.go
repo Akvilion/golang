7810438ddf095477cede7efdb655f363306a428e
Akvilion <ianko.andrii@gmail.com>
2024-04-23 14:37:14 +0300

    87

---
+++ types/seven.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type Liters float64
+type Milliliters float64
+type Gallons float64
+
+func (l Liters) ToMilliliters() Milliliters {
+	return Milliliters(l * 1000)
+}
+func (m Milliliters) ToLiters() Liters {
+	return Liters(m / 1000)
+}
+
+func main() {
+	l := Liters(3)
+	fmt.Printf("%0.1f liters is %0.1f milliliters\n", l, l.ToMilliliters())
+	ml := Milliliters(500)
+	fmt.Printf("%0.1f milliliters is %0.1f liters\n", ml, ml.ToLiters())
+}
+
+// 3.0 liters is 3000.0 milliliters
+// 500.0 milliliters is 0.5 liters
