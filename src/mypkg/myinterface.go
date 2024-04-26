33d0e80aececa87ee66aa2276da3a1fd86203d62
Akvilion <ianko.andrii@gmail.com>
2024-04-26 22:51:37 +0300

    96

---
+++ src/mypkg/myinterface.go
@@ -0,0 +1,23 @@
+package mypkg // назва йде за назвою папки
+import "fmt"
+
+type MyInterface interface {
+	MethodWithoutParameters()
+	MethodWithParameter(float64)
+	MethodWithReturnValue() string
+}
+
+type MyInt int
+
+func (m MyType) MethodWithoutParameters() {
+	fmt.Println("MethodWithoutParameters called")
+}
+func (m MyType) MethodWithParameter(f float64) {
+	fmt.Println("MethodWithParameter called with", f)
+}
+func (m MyType) MethodWithReturnValue() string {
+	return "Hi from MethodWithReturnValue"
+}
+func (my MyType) MethodNotInInterface() {
+	fmt.Println("MethodNotInInterface called")
+}
