a8727feea44be869aa5fa7f349097be7061bd9f0
Akvilion <ianko.andrii@gmail.com>
2024-05-10 09:34:53 +0300

    135

--- gorutines_414/430.go
+++ gorutines_414/430.go
@@ -19,12 +19,12 @@ func responseSize(url string, chanel chan Page) {
 		log.Fatal(err)
 	}
 	// chanel <- len(body)
-	chanel <- Page{page: url, size: len(body)}
+	chanel <- Page{Page: url, Size: len(body)}
 }
 
 type Page struct {
-	page string
-	size int
+	Page string
+	Size int
 }
 
 func main() {
@@ -34,7 +34,12 @@ func main() {
 	for _, v := range urls {
 		go responseSize(v, myChan)
 	}
-	fmt.Println(<-myChan)
-	fmt.Println(<-myChan)
-	fmt.Println(<-myChan)
+	for i := 0; i < len(urls); i++ {
+		page := <-myChan
+		fmt.Printf("%s: %d\n", page.Page, page.Size)
+	}
+
+	// fmt.Println(<-myChan)
+	// fmt.Println(<-myChan)
+	// fmt.Println(<-myChan)
 }
