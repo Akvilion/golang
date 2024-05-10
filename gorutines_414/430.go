8e7fd6907b5dc539b181313ae7e3bc146653c8b8
Akvilion <ianko.andrii@gmail.com>
2024-05-10 09:31:38 +0300

    134

--- gorutines_414/430.go
+++ gorutines_414/430.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 )
 
-func responseSize(url string, chanel chan int) {
+func responseSize(url string, chanel chan Page) {
 	fmt.Println("Getting", url)
 	response, err := http.Get(url)
 	if err != nil {
@@ -18,11 +18,17 @@ func responseSize(url string, chanel chan int) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	chanel <- len(body)
+	// chanel <- len(body)
+	chanel <- Page{page: url, size: len(body)}
+}
+
+type Page struct {
+	page string
+	size int
 }
 
 func main() {
-	myChan := make(chan int)
+	myChan := make(chan Page)
 
 	urls := [3]string{"https://example.com/", "https://golang.org/", "https://golang.org/doc"}
 	for _, v := range urls {
