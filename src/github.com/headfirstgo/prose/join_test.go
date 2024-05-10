64b49e87570220372b3f105d9bf03052c0ad132b
Akvilion <ianko.andrii@gmail.com>
2024-05-10 11:19:09 +0300

    138

--- src/github.com/headfirstgo/prose/join_test.go
+++ src/github.com/headfirstgo/prose/join_test.go
@@ -4,6 +4,10 @@ import "testing"
 
 func TestTwoElements(t *testing.T) {
 	//t.Error("no test written yet")
+	list := []string{"apple", "orange"}
+	if JoinWithCommas(list) != "apple and orange" {
+		t.Error("didn't match expected value")
+	}
 }
 func TestThreeElements(t *testing.T) {
 	//t.Error("no test written yet")
