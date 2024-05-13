b6b717e285444f7313798f1bce3c86af469eddcc
Akvilion <ianko.andrii@gmail.com>
2024-05-13 10:15:47 +0300

    150

--- web/hello.go
+++ web/hello.go
@@ -5,12 +5,16 @@ import (
 	"net/http"
 )
 
-func viewHandler(response http.ResponseWriter, request *http.Request) {
-	message := []byte("Hello world of GO!")
+func writeMessage(response http.ResponseWriter, test string) {
+	message := []byte(test)
 	_, err := response.Write(message)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func viewHandler(response http.ResponseWriter, request *http.Request) {
+	writeMessage(response, "Hello World")
 }
 
 func main() {
