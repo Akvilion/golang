9b0c490f56474ea9bf9d0d1af171607de13f551b
Akvilion <ianko.andrii@gmail.com>
2024-05-15 11:36:54 +0300

    170

--- myproject/guestbook.go
+++ myproject/guestbook.go
@@ -60,6 +60,7 @@ func createHandler(writer http.ResponseWriter, request *http.Request) {
 	check(err)
 	err = file.Close()
 	check(err)
+	http.Redirect(writer, request, "/guestbook", http.StatusFound)
 
 }
 
