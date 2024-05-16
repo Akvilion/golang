f07fb5f90e6b40157c7e4b9b4b6bc5d62f063210
Akvilion <ianko.andrii@gmail.com>
2024-05-16 21:09:53 +0300

    177

---
+++ rune/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	asciiString := "ABCDE"
+	utf8String := "БГДЖИ"
+	// неправильний варіант, тому, що кирилиця записується в 2 байти
+	asciiBytes := []byte(asciiString)
+	utf8Bytes := []byte(utf8String)
+	asciiBytesPartial := asciiBytes[3:]
+	utf8BytesPartial := utf8Bytes[3:]
+	fmt.Println(string(asciiBytesPartial))
+	fmt.Println(string(utf8BytesPartial))
+
+	// правильний варіант для роботи з кирилицею а не ascii
+	asciiRunes := []rune(asciiString)
+	utf8Runes := []rune(utf8String)
+	asciiRunesPartial := asciiRunes[3:]
+	utf8RunesPartial := utf8Runes[3:]
+	fmt.Println(string(asciiRunesPartial))
+	fmt.Println(string(utf8RunesPartial))
+}
