34d58cca51232294d21a628ab3b5fe7c255d66b7
Akvilion <ianko.andrii@gmail.com>
2024-05-16 20:55:22 +0300

    176

---
+++ switch/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	switch rand.Intn(3) + 1 {
+	case 1:
+		fmt.Println("You win a cruise!")
+	case 2:
+		fmt.Println("You win a car!")
+	case 3:
+		fmt.Println("You win a goat!")
+	default:
+		panic("invalid door number")
+	}
+}
