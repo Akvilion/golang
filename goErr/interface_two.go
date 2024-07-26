1f8860ee6218193340ebb6ed0f0e4ef81b5fd314
Akvilion <ianko.andrii@gmail.com>
2024-07-26 16:35:54 +0300

    195

--- goErr/interface_two.go
+++ goErr/interface_two.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-type vehicleMove interface { // задаємо інтерфейс
+type vehicleMove interface { // задаємо інтерфейс який повинен гарантувати реалізацію методу move()
 	move()
 }
 
