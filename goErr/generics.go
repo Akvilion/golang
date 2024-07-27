c3e6e1e15c7f55efae9af92fc5c50ad3901f04e7
Akvilion <ianko.andrii@gmail.com>
2024-07-27 20:33:04 +0300

    199

---
+++ goErr/generics.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func one(x interface{}) {
+	switch t := x.(type) {
+	case int:
+		fmt.Printf("Type is int with value %d\n", t)
+	case string:
+		fmt.Printf("Type is string with value %s\n", t)
+	default:
+		fmt.Printf("Unknown type\n")
+	}
+}
+
+func main() {
+	one(42)
+	one("hello")
+	one(3.14)
+}
