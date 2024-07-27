e66042db200a94924d24735fc68336e9ae228430
Akvilion <ianko.andrii@gmail.com>
2024-07-27 20:50:57 +0300

    202

--- goErr/generics_two.go
+++ goErr/generics_two.go
@@ -5,8 +5,10 @@ import "fmt"
 // до го 1.18 використовувати пустий інтерфейс interface{}
 // а з го 1.18 використовують any
 
+// мінус такого підходу це те що тип знається не на етапі компіляції а на етапі вже виконання програми
+
 func getKeys(m any) ([]any, error) {
-	switch t := m.(type) {
+	switch t := m.(type) { // тип взнається вже під час виконання програми
 	default:
 		return nil, fmt.Errorf("unknown type: %T", t)
 	case map[string]int:
