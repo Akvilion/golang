ea8a5a4f5ed07064d75d185e1a3c275a95e3c142
Akvilion <ianko.andrii@gmail.com>
2024-08-19 19:02:14 +0300

    251

---
+++ goErr/ctx.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		// Скасовуємо операцію через 2 секунди
+		time.Sleep(2 * time.Second)
+		cancel()
+	}()
+
+	doSomething(ctx)
+}
+
+func doSomething(ctx context.Context) {
+	select {
+	case <-time.After(5 * time.Second):
+		fmt.Println("Operation completed")
+	case <-ctx.Done():
+		// ctx.Done() канал сповіщає про скасування контексту
+		fmt.Println("Operation canceled:", ctx.Err())
+	}
+}
