02f51896d7adb0b666277df435e6f5778b058946
Akvilion <ianko.andrii@gmail.com>
2024-10-04 17:12:59 +0000

    268

--- learn-to-code-go-version-03/000-ac-interface-05-assetion-01/main.go
+++ learn-to-code-go-version-03/000-ac-interface-05-assetion-01/main.go
@@ -7,7 +7,11 @@ import (
 func main() {
 	var s1 interface{} = "James"
 	var s2 any = "Bond"
+	var s3 any = 21
 
-	fmt.Println(s1.(string))
-	fmt.Println(s2.(string))
+	fmt.Println(s1.(string)) // type assertion
+	fmt.Println(s2.(string)) // type assertion
+	fmt.Println(s3.(int))    // type assertion
+
+	// якщо тип підтверджується то type assertion просто повертає значення
 }
