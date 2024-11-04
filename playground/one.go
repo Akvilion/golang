3e0e16c0365d7f8efa154eb436679df9e04d969b
Akvilion <ianko.andrii@gmail.com>
2024-11-04 19:41:20 +0200

    289

--- playground/one.go
+++ playground/one.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
+// можна використовувати як локально (всередині функцій) так поза ними
+var f int = 22
+
+// можна використувувати тільки всередині функцій
+// f:=22
+
 func main() {
 	ch := make(chan int)
-
 	go one(ch)
 
 	<-ch
@@ -14,6 +19,8 @@ func main() {
 }
 
 func one(ch chan int) {
+
+	fmt.Println(f)
 	defer close(ch)
 	fmt.Printf("one -> ch")
 }
